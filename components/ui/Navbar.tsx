ae8253ad52a7678767b4f3995f77bcd54c20a879
MauricioTejada <mauriciotejada14@gmail.com>
2023-11-10 21:51:58 -0300

    acepta imágenes de páginas externas

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,5 +1,14 @@
+import { useTheme } from "next-themes";
+import { Spacer } from "@nextui-org/react";
+import Image from "next/image";
+
 
 export const Navbar = () => {
+    const { theme } = useTheme();
+
+    console.log(theme);
+
+
   return (
     <div style={{
         display:'flex',
@@ -8,9 +17,20 @@ export const Navbar = () => {
         alignItems: 'center',
         justifyContent: 'start',
         padding: '0x 20px',
-        backgroundColor: 'red'
+        // backgroundColor: theme?.colors.gray900.value
     }}>
-        <span>Hola</span>
+
+        <Image
+            src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+            alt="icono de la app"
+            width={70}
+            height={70}
+        />
+
+        <text>P</text>
+        <text>ókemon</text>
+        <Spacer style={{ flex: 1}}/>
+        <text> Favoritos </text>
     </div>
   )
 }
